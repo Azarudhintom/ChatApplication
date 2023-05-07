73b4b96124feca28bc14350bfa273ef134c956e1
Azarudhintom <azarudhinproject@gamil.com>
2023-05-07 14:20:28 +0530

    data bas ewill be Changed

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,31 @@
+// import { initializeApp } from "firebase/app";
+// 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyApQ5P1lZIf-UqAymbaJAGNM8EWnxh3CAM",
+//   authDomain: "chat-application-c5788.firebaseapp.com",
+//   projectId: "chat-application-c5788",
+//   storageBucket: "chat-application-c5788.appspot.com",
+//   messagingSenderId: "994221682360",
+//   appId: "1:994221682360:web:642b558d2e27c5d5b48e4d"
+// };
+
+// export const app = initializeApp(firebaseConfig);
+
+
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyApQ5P1lZIf-UqAymbaJAGNM8EWnxh3CAM",
-  authDomain: "chat-application-c5788.firebaseapp.com",
-  projectId: "chat-application-c5788",
-  storageBucket: "chat-application-c5788.appspot.com",
-  messagingSenderId: "994221682360",
-  appId: "1:994221682360:web:642b558d2e27c5d5b48e4d"
+  apiKey: "AIzaSyCHuxhuTBpvOgvBQ7Mf0Fag7sab9ipx_FY",
+  authDomain: "chatappliaction-f64d3.firebaseapp.com",
+  projectId: "chatappliaction-f64d3",
+  storageBucket: "chatappliaction-f64d3.appspot.com",
+  messagingSenderId: "298516451814",
+  appId: "1:298516451814:web:81d22a4c8984910214c627"
 };
 
+// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
 export const storage = getStorage();
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -22,12 +22,12 @@ const Login = () => {
   return (
     <div className="formContainer">
       <div className="formWrapper">
-        <span className="logo">Lama Chat</span>
+        <span className="logo">Chat Application</span>
         <span className="title">Login</span>
         <form onSubmit={handleSubmit}>
           <input type="email" placeholder="email" />
           <input type="password" placeholder="password" />
-          <button>Sign in</button>
+          <button>Login</button>
           {err && <span>Something went wrong</span>}
         </form>
         <p>You don't have an account? <Link to="/register">Register</Link></p>
